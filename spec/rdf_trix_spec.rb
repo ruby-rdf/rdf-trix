6f35661e0f9bcf05986cb6599be180265e3f7d3d
Arto Bendiken <arto@bendiken.net>
2010-03-11 22:04:32 +0100

    Added initial specs for RDF::TriX::Reader.

--- spec/rdf_trix_spec.rb
+++ spec/rdf_trix_spec.rb
@@ -1,5 +1,19 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe RDF::TriX do
+describe RDF::TriX::Reader do
+  context "when parsing etc/doap.xml" do
+    before :each do
+      etc = File.expand_path(File.join(File.dirname(__FILE__), '..', 'etc'))
+      @ntriples = RDF::NTriples::Reader.new(File.open(File.join(etc, 'doap.nt')))
+      @reader = RDF::TriX::Reader.open(File.join(etc, 'doap.xml'))
+    end
+
+    it "should return the correct number of statements" do
+      @reader.count.should == @ntriples.count
+    end
+  end
+end
+
+describe RDF::TriX::Writer do
   # TODO
 end
