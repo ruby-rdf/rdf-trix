88b013752657000e7ee4577d1c9f2a35b13d582a
Arto Bendiken <arto@bendiken.net>
2010-03-12 02:57:12 +0100

    Added specs for format autodiscovery.

--- spec/rdf_trix_spec.rb
+++ spec/rdf_trix_spec.rb
@@ -1,6 +1,30 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
+describe RDF::TriX::Format do
+  it "should be discoverable" do
+    formats = [
+      RDF::Format.for(:trix),
+      RDF::Format.for("etc/doap.xml"),
+      RDF::Format.for(:file_name      => "etc/doap.xml"),
+      RDF::Format.for(:file_extension => "xml"),
+      RDF::Format.for(:content_type   => "application/trix"),
+    ]
+    formats.each { |format| format.should == RDF::TriX::Format }
+  end
+end
+
 describe RDF::TriX::Reader do
+  it "should be discoverable" do
+    readers = [
+      RDF::Reader.for(:trix),
+      RDF::Reader.for("etc/doap.xml"),
+      RDF::Reader.for(:file_name      => "etc/doap.xml"),
+      RDF::Reader.for(:file_extension => "xml"),
+      RDF::Reader.for(:content_type   => "application/trix"),
+    ]
+    readers.each { |reader| reader.should == RDF::TriX::Reader }
+  end
+
   context "when parsing etc/doap.xml" do
     before :each do
       etc = File.expand_path(File.join(File.dirname(__FILE__), '..', 'etc'))
@@ -15,5 +39,14 @@ describe RDF::TriX::Reader do
 end
 
 describe RDF::TriX::Writer do
-  # TODO
+  it "should be discoverable" do
+    writers = [
+      RDF::Writer.for(:trix),
+      RDF::Writer.for("etc/test.xml"),
+      RDF::Writer.for(:file_name      => "etc/test.xml"),
+      RDF::Writer.for(:file_extension => "xml"),
+      RDF::Writer.for(:content_type   => "application/trix"),
+    ]
+    writers.each { |writer| writer.should == RDF::TriX::Writer }
+  end
 end
